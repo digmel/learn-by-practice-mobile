32bc0b6e52dfe66aa2faf3e0033407bd406e613f
George <gio.digmel@gmail.com>
2021-10-15 14:13:25 -0700

    refactoring get started screen

--- first-aid-mobile/src/screens/getStarted/GetStartedScreen.view.tsx
+++ first-aid-mobile/src/screens/getStarted/GetStartedScreen.view.tsx
@@ -1,7 +1,8 @@
 import React, {FC} from 'react';
 import {styles} from './GetStartedScreen.style';
 import {TGetStartedScreenViewProps} from './GetStartedScreen.type';
-import {Screen, Button, Section, Text, ScreenUnprotected} from '@components';
+import {Screen, Button, Section, Text, color} from '@components';
+import {Image} from 'react-native';
 
 export const GetStartedScreenView: FC<TGetStartedScreenViewProps> = ({
   goToLogin,
@@ -9,18 +10,30 @@ export const GetStartedScreenView: FC<TGetStartedScreenViewProps> = ({
 }) => {
   return (
     <Screen isScrollViewDisabled>
-      <ScreenUnprotected>
-        <Section>
-          <Text>Welcome to First Aid App</Text>
-        </Section>
-        <Section>
-          <Button text="Login" onPress={goToLogin} />
-        </Section>
+      <Section
+        containerStyle={styles.imageContainer}
+        topSpace={32}
+        bottomSpace={64}>
+        <Image
+          source={require('../../assets/images/get-started-screen.png')}></Image>
+      </Section>
 
-        <Section>
-          <Button text="Sign Up" variation="secondary" onPress={goToSignUp} />
-        </Section>
-      </ScreenUnprotected>
+      <Section bottomSpace={64}>
+        <Text variation="H1" fontWeight="extraBold" color={color.primary100}>
+          Learn how to act
+        </Text>
+        <Text variation="H4" fontWeight="light" color={color.gray500}>
+          When every second is important!
+        </Text>
+      </Section>
+
+      <Section>
+        <Button text="Login" onPress={goToLogin} />
+      </Section>
+
+      <Section>
+        <Button text="Sign Up" variation="secondary" onPress={goToSignUp} />
+      </Section>
     </Screen>
   );
 };
