90cc47bbb841c0f4ff11777a88748a7511d0dbf1
George <gio.digmel@gmail.com>
2021-10-17 14:30:35 -0700

    adding variation in section component

--- first-aid-mobile/src/components/section/Section.tsx
+++ first-aid-mobile/src/components/section/Section.tsx
@@ -1,16 +1,17 @@
 import React, {FC} from 'react';
-import {View, ViewStyle, StyleProp} from 'react-native';
+import {View, ViewStyle} from 'react-native';
 import {styles} from './Section.style';
 import {TSectionProps} from './Section.type';
-import {size} from '@components';
+import {color, size} from '@components';
 
 export const Section: FC<TSectionProps> = ({
+  variation,
   containerStyle,
   isFullWidth = false,
   children,
   topSpace,
   bottomSpace,
-  ...props
+  contentStyle,
 }) => {
   const fullWidthStyle = isFullWidth && {marginHorizontal: -size.xl};
 
@@ -20,17 +21,43 @@ export const Section: FC<TSectionProps> = ({
     ? {marginBottom: bottomSpace}
     : {marginBottom: size.m};
 
-  const containerDynamicStyle: StyleProp<ViewStyle> = {
+  const variationStyle: ViewStyle = {};
+
+  const contentDynamicStyle: ViewStyle = {};
+
+  switch (variation) {
+    case 'primary':
+      variationStyle.backgroundColor = color.primary;
+      variationStyle.paddingVertical = size.l;
+      variationStyle.borderBottomRightRadius = size.xl;
+      variationStyle.borderTopRightRadius = 2 * size.xxl;
+      variationStyle.marginRight = size.xl;
+      variationStyle.marginHorizontal = -size.xl;
+      contentDynamicStyle.marginHorizontal = size.xl;
+      break;
+    case 'secondary':
+      variationStyle.backgroundColor = color.accent;
+      variationStyle.paddingVertical = size.l;
+      variationStyle.borderBottomLeftRadius = size.xl;
+      variationStyle.borderTopLeftRadius = 2 * size.xxl;
+      variationStyle.marginLeft = size.xl;
+      variationStyle.marginHorizontal = -size.xl;
+      contentDynamicStyle.marginHorizontal = size.xl;
+      break;
+  }
+
+  const containerDynamicStyle = {
+    ...variationStyle,
     ...fullWidthStyle,
     ...topSpaceStyle,
     ...bottomSpaceStyle,
   };
 
   return (
-    <View
-      style={[styles.container, containerDynamicStyle, containerStyle]}
-      {...props}>
-      {children}
+    <View style={[styles.container, containerDynamicStyle, containerStyle]}>
+      <View style={[styles.content, contentDynamicStyle, contentStyle]}>
+        {children}
+      </View>
     </View>
   );
 };
--- first-aid-mobile/src/components/section/Section.type.ts
+++ first-aid-mobile/src/components/section/Section.type.ts
@@ -1,8 +1,12 @@
 import type {ReactNode} from 'react';
 import type {ViewStyle, StyleProp} from 'react-native';
 
+export type TSectionVariation = 'primary' | 'secondary';
+
 export type TSectionProps = {
+  variation?: TSectionVariation;
   containerStyle?: StyleProp<ViewStyle>;
+  contentStyle?: StyleProp<ViewStyle>;
   children: ReactNode;
   isFullWidth?: boolean;
   topSpace?: number;
