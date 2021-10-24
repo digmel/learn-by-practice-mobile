c9e5b768fbe80d8b3a62a3c9f98b147ca7c030ba
George <gio.digmel@gmail.com>
2021-10-24 16:23:15 -0700

    adjusting ui

--- first-aid-mobile/src/components/answer/Answer.tsx
+++ first-aid-mobile/src/components/answer/Answer.tsx
@@ -1,5 +1,5 @@
 import React, {FC} from 'react';
-import {View, ViewStyle, TouchableOpacity} from 'react-native';
+import {View, TouchableOpacity} from 'react-native';
 import {styles} from './Answer.style';
 import {TAnswerProps} from './Answer.type';
 import {color, size, Text} from '@components';
@@ -8,32 +8,27 @@ export const Answer: FC<TAnswerProps> = ({
   answer,
   variation = 'regular',
   onPress,
-  containerStyle,
 }) => {
-  const variationStyle: ViewStyle = {};
-
-  const contentDynamicStyle: ViewStyle = {};
+  let dynamicColor;
 
   switch (variation) {
     case 'regular':
+      dynamicColor = color.primary;
       break;
     case 'wrong':
-      variationStyle.backgroundColor = color.error;
+      dynamicColor = color.error;
       break;
     case 'correct':
-      variationStyle.backgroundColor = color.success;
+      dynamicColor = color.success;
       break;
   }
 
-  const containerDynamicStyle = {
-    ...variationStyle,
-  };
-
   return (
     <TouchableOpacity
-      style={[styles.container, containerDynamicStyle, containerStyle]}>
-      <View style={[styles.content]}>
-        <Text variation="caption" fontWeight="light">
+      style={styles.container}
+      onPress={() => onPress && onPress()}>
+      <View style={styles.content}>
+        <Text variation="caption" fontWeight="light" color={dynamicColor}>
           {answer}
         </Text>
       </View>
