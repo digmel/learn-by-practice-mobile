536fc422dafe66d3e46553abe8c883457472edc2
George <gio.digmel@gmail.com>
2021-11-07 17:34:08 -0800

    updating cards

--- first-aid-mobile/src/components/card/Card.type.ts
+++ first-aid-mobile/src/components/card/Card.type.ts
@@ -2,10 +2,6 @@ import {ViewStyle, StyleProp, ImageSourcePropType} from 'react-native';
 
 export type TCardProps = {
   title: String;
-  image?: ImageSourcePropType;
+  svg?: ImageSourcePropType;
   onPress?: Function;
-  titleContainerStyle?: StyleProp<ViewStyle>;
-  imageContainerStyle?: StyleProp<ViewStyle>;
-  cardStyle?: StyleProp<ViewStyle>;
-  cardContainerStyle?: StyleProp<ViewStyle>;
 };
--- first-aid-mobile/src/components/cardSlider/CardSlider.tsx
+++ first-aid-mobile/src/components/cardSlider/CardSlider.tsx
@@ -1,7 +1,7 @@
 import React, {FC} from 'react';
 import {TCardSliderProps} from './CardSlider.type';
 import {styles} from './CardSlider.style';
-import {Text, color} from '@components';
+import {Text} from '@components';
 import {ScrollView, View} from 'react-native';
 
 export const CardSlider: FC<TCardSliderProps> = ({
@@ -14,8 +14,8 @@ export const CardSlider: FC<TCardSliderProps> = ({
   return (
     <View style={styles.wrapper}>
       <View style={[styles.titleContainer, titleContainerStyle]}>
-        <Text>{title}</Text>
-        <Text>{subTitle}</Text>
+        <Text variation="H1_bold">{title}</Text>
+        <Text variation="H1_light">{subTitle}</Text>
       </View>
       <ScrollView
         horizontal
