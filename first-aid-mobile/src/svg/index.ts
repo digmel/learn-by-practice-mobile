19c713ef47ec6191e92736912ff7ba6602bfadc9
George <gio.digmel@gmail.com>
2022-02-11 15:56:31 -0800

    adding diabetes svgs

--- first-aid-mobile/src/svg/index.ts
+++ first-aid-mobile/src/svg/index.ts
@@ -1,6 +1,5 @@
 export * from './Logo';
 export * from './IntroductionScreenSvg';
-export * from './DiabetesSvg';
 export * from './FracturesSvg';
 export * from './EarthquakeSvg';
 export * from './ThunderstormSvg';
@@ -21,3 +20,9 @@ export * from './HeartAttackSvg';
 export * from './HeartAttackSvg01';
 export * from './HeartAttackSvg02';
 export * from './HeartAttackSvg03';
+
+export * from './DiabetesSvg';
+export * from './DiabetesSvg01';
+export * from './DiabetesSvg02';
+export * from './DiabetesSvg03';
+export * from './DiabetesSvg04';
