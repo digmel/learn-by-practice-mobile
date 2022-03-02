64456ea4183344660d62c85c08ae4e7135e5cdff
George <gio.digmel@gmail.com>
2022-03-01 23:08:00 -0800

    adding disabled color

--- first-aid-mobile/src/components/button/Button.tsx
+++ first-aid-mobile/src/components/button/Button.tsx
@@ -49,7 +49,7 @@ export const Button: FC<TButtonProps> = ({
   switch (variation) {
     case 'primary':
       dynamicProperties.backgroundColor = isDisabled
-        ? color.primary
+        ? color.disabled
         : color.primary;
       dynamicProperties.borderWidth = 0;
       dynamicProperties.textColor = color.light;
@@ -58,9 +58,11 @@ export const Button: FC<TButtonProps> = ({
     case 'secondary':
       dynamicProperties.backgroundColor = 'transparent';
       dynamicProperties.borderWidth = BORDER_OUTLINE_WIDTH;
-      dynamicProperties.textColor = color.primary;
-      dynamicProperties.borderColor = color.primary;
-      dynamicProperties.iconColor = color.primary;
+      dynamicProperties.textColor = isDisabled ? color.disabled : color.primary;
+      dynamicProperties.borderColor = isDisabled
+        ? color.disabled
+        : color.primary;
+      dynamicProperties.iconColor = isDisabled ? color.disabled : color.primary;
       break;
     case 'link':
       dynamicProperties.backgroundColor = isDisabled
--- first-aid-mobile/src/components/config/color.ts
+++ first-aid-mobile/src/components/config/color.ts
@@ -9,6 +9,8 @@ export const color = {
 
   error: '#F20E56',
 
+  disabled: '#84A5B0',
+
   gray100: '#F4F5F6',
   gray200: '#E3E5E8',
   gray300: '#D5D9DC',
